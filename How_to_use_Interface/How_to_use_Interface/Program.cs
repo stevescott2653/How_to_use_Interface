ceab0a32326f6285e06748ebb49453c5b16c33a3
Steven Scott <stevescott2653@gmail.com>
2019-10-01 16:56:12 -0400

    Add files via upload

---
+++ How_to_use_Interface/How_to_use_Interface/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace How_to_use_Interface
+{
+    public interface Drawable
+    {
+        void Draw();
+    }
+
+    public class Rectangle : Drawable
+    {
+        public void Draw()
+        {
+            Console.WriteLine("drawing rectangle...");
+        }
+    }
+
+    public class Circle : Drawable
+    {
+        public void Draw()
+        {
+            Console.WriteLine("drawing circle...");
+        }
+    }
+
+    public class TestInterface
+    {
+        public static void Main()
+        {
+            Drawable d;
+            d = new Circle();
+            d.Draw();
+            d = new Circle();
+            d.Draw();
+        }
+    }
+}
